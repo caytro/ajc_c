92195cbe888bfbd890a7ade5b0bc00b82855e326
sylvain.nadaud <snadaud@free.fr>
2023-07-24 17:33:28 +0200

    jour2_apresMidi

--- projet_1/main.c
+++ projet_1/main.c
@@ -3,53 +3,65 @@
 #include <float.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include "mesConst.h"
+
 
 
 
 /**
- * @brief main affiche mon nom
+ * @brief Exercice
  * @return 0 si OK et 1 en cas de pb
  */
 
-void tailleMemoire()
-{
-    printf("-- Taille mémoire --\n");
-    printf("Un octet se compose de %d bits.\n", CHAR_BIT);
-    printf("signed char : min = %d ; max = %d.\n", SCHAR_MIN, SCHAR_MAX);
-    printf("unsigned char : min = 0 ; max = %u.\n", UCHAR_MAX);
-    printf("short : min = %d ; max = %d.\n", SHRT_MIN, SHRT_MAX);
-    printf("unsigned short : min = 0 ; max = %u.\n", USHRT_MAX);
-    printf("int : min = %d ; max = %d.\n", INT_MIN, INT_MAX);
-    printf("unsigned int : min = 0 ; max = %u.\n", UINT_MAX);
-    printf("long : min = %ld ; max = %ld.\n", LONG_MIN, LONG_MAX);
-    printf("unsigned long : min = 0 ; max = %lu.\n", ULONG_MAX);
-    printf("long long : min = %lld ; max = %lld.\n", LLONG_MIN, LLONG_MAX);
-    printf("unsigned long long : min = 0 ; max = %lld.\n", ULLONG_MAX);
-    printf("unsigned double : min = 0 ; max = %lld.\n", ULLONG_MAX);
-    printf("long long : min = %lld ; max = %lld.\n", LLONG_MIN, LLONG_MAX);
-    printf("float : min = %e ; max = %e.\n", FLT_MIN, FLT_MAX);
-    printf("double : min = %e ; max = %e.\n", DBL_MIN, DBL_MAX);
-    printf("long double : min = %Le ; max = %Le.\n", LDBL_MIN, LDBL_MAX);
-
-    printf("---------------------------\n");
+int max(int n1, int n2){
+    return (n1>=n2 ? n1 :n2);
 }
 
-float absolute(float x)
-{
-    return (x>=0?x:-x);
+int min(int n1, int n2){
+    return (n1<=n2 ? n1 :n2);
 }
 
+int ascii(char c){
+    return (int)c;
+}
+
+unsigned char Char(int i){
+    return ((unsigned char)i);
+}
+
+char* premmajuscule(char *chaine){
+    if (chaine[0]>='a' && chaine[0]<'z') chaine[0]-=32;
+    return chaine;
+}
+
+char* majuscule(char *chaine){
+    for (unsigned long i =0;i<strlen(chaine);i++){
+        if (chaine[i]>='a' && chaine[i]<'z') chaine[i]-=32;
+    }
+    return chaine;
+}
+
+char* minuscule(char *chaine){
+    for (unsigned long i =0;i<strlen(chaine);i++){
+        if (chaine[i]>='A' && chaine[i]<'Z') chaine[i]+=32;
+    }
+    return chaine;
+}
 int main()
 {
-    tailleMemoire();
-    long long puissance2[65];
-    puissance2[0]=1;
-    for (int i=1;i<65;i++)
-    {
-        puissance2[i]=puissance2[i-1]*2;
-        printf("\n%d : %lld = %e\n",i,puissance2[i], (float)puissance2[i]);
-    }
+    int tmp;
 
-
+    printf("\nmin(10,9)=%d\n", min(10,9));
+    printf("\nascii('a')=%d", ascii('a'));
+    printf("\nChar(512+97)='%c'", Char(512+97));
+    char chaine[50]="bonjour";
+    printf("\npremmajuscule(bonjour) : %s",premmajuscule(chaine));
+    strcpy(chaine,"bonjour tout le monde 786");
+    printf("\nmajuscule(%s) : %s","bonjour tout le monde 786",majuscule(chaine));
+    strcpy(chaine,"BONJOUR TOUT le MONDE 786");
+    printf("\nminuscule(%s) : %s","BONJOUR TOUT le MONDE 786",minuscule(chaine));
+    printf("\n");
+    PRINT_MY_NAME;
     return 0;
 }
---
+++ projet_1/mesConst.h
@@ -0,0 +1,6 @@
+#ifndef MESCONST_H
+#define MESCONST_H
+#define NOM "NADAUD"
+#define PRENOM "Sylvain"
+#define PRINT_MY_NAME printf("\n %s %s \n",NOM,PRENOM);
+#endif // MESCONST_H
